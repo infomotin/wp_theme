a35cf9fdc4791a10e827a6377f0238164c006d73
Abdul Motin <motin.mmk.jr@gmail.com>
2020-11-17 18:21:09 +0600

    theme support customize plugin custom post with meta data create db

--- wordpress-5.5/wordpress/wp-content/plugins/recipe/include/activate.php
+++ wordpress-5.5/wordpress/wp-content/plugins/recipe/include/activate.php
@@ -3,19 +3,21 @@ function r_activate_plugin(){
     if(version_compare(get_bloginfo('version'),'5.0','<')){
        wp_die(__("Your WP are not supoort",'recipe_dazzle')); 
     }
-}
+
 
 
 //crate db on wp 
 global $wpdb;
 $createSQl ="
-CREATE TABLE `wp`.`wp_recipe_ratings` ( 
-    `ID` BIGINT(20) NOT NULL AUTO_INCREMENT , 
-    `recipe_id` BIGINT(20) NOT NULL ,
-    `rating` FLOAT(3,2) NOT NULL ,
-    `user_ip` VARCHAR(50) NOT NULL ,
-    PRIMARY KEY (`ID`)
-    ) ENGINE = InnoDB;"
-
+CREATE TABLE " . $wpdb->prefix . "recipe_ratings ( 
+    ID BIGINT(20) NOT NULL AUTO_INCREMENT , 
+    recipe_id BIGINT(20) NOT NULL ,
+    rating FLOAT(3,2) NOT NULL ,
+    user_ip VARCHAR(50) NOT NULL ,
+    PRIMARY KEY (ID)
+    ) ENGINE = InnoDB" . $wpdb->get_charset_collate() . ";";
+    require(ABSPATH . "/wp-admin/includes/upgrade.php");
+    dbDelta($createSQl);
+}
 
 ?>
