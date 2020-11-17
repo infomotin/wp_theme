90d3bcd17f80793c520c2e50d8786c0cd0f299c9
Abdul Motin <motin.mmk.jr@gmail.com>
2020-11-17 16:02:03 +0600

    theme support customize plugin custom post with meta data

--- wordpress-5.5/wordpress/wp-content/plugins/recipe/process/save-post.php
+++ wordpress-5.5/wordpress/wp-content/plugins/recipe/process/save-post.php
@@ -1,6 +1,11 @@
 <?php 
 function r_save_post_admin($post_id,$post,$update){
     $recipe_date = get_post_meta($post_id,'recipe_date',true);
+    $recipe_date = empty($recipe_date)?[]:$recipe_date;
+    $recipe_date['rating'] = isset($recipe_date['rating'])? absint( $recipe_date['rating'] ): 0 ;
+    $recipe_date['rating_count'] = isset($recipe_date['rating_count'])? absint( $recipe_date['rating_count'] ): 0 ;
+
+    update_post_meta($post_id,'recipe_date',$recipe_date);
 }
 
 
