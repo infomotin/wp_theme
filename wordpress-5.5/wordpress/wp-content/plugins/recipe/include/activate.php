d2e04487ff106cfa0308b28b50c2067948ee86c1
Abdul Motin <motin.mmk.jr@gmail.com>
2020-11-17 18:26:59 +0600

    theme support customize plugin custom post with meta data create db update setteting change

--- wordpress-5.5/wordpress/wp-content/plugins/recipe/include/activate.php
+++ wordpress-5.5/wordpress/wp-content/plugins/recipe/include/activate.php
@@ -8,14 +8,23 @@ function r_activate_plugin(){
 
 //crate db on wp 
 global $wpdb;
+$prefix = $wpdb->prefix;
+$charset_collate = "";
+
+        if ( !empty($wpdb->charset) )
+            $charset_collate = "DEFAULT CHARACTER SET {$wpdb->charset} ";
+
+        if ( !empty($wpdb->collate) )
+            $charset_collate .= "COLLATE {$wpdb->collate}";
+
 $createSQl ="
-CREATE TABLE " . $wpdb->prefix . "recipe_ratings ( 
+CREATE TABLE {$prefix}recipe_ratings ( 
     ID BIGINT(20) NOT NULL AUTO_INCREMENT , 
     recipe_id BIGINT(20) NOT NULL ,
     rating FLOAT(3,2) NOT NULL ,
     user_ip VARCHAR(50) NOT NULL ,
     PRIMARY KEY (ID)
-    ) ENGINE = InnoDB" . $wpdb->get_charset_collate() . ";";
+    ) {$charset_collate} ENGINE = InnoDB;";
     require(ABSPATH . "/wp-admin/includes/upgrade.php");
     dbDelta($createSQl);
 }
